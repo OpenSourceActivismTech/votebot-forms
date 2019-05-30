6f48ca41deb2e999901226974f51e789b767110a
Peter Darche <pdarche@gmail.com>
2019-05-29 20:29:45 -0700

    Update dockerfiles and docker-compose to deploy to Google Cloud
      * Create separate dockerfiles for web and worker
      * Create docker-compose-prod file to deploy to kubernetes using
      kompose

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ DEBUG = os.environ.get('DEBUG', True)
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://redis:6379/')
 RQ_REDIS_URL = REDIS_URL
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:password@db/votebot-forms')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:password@db/postgres')
 VOTEBOT_API_KEY = os.environ.get('VOTEBOT_API_KEY', None)
 
 AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
